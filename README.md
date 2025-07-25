81d3172358e0738c685638d6a55ffa8cafc9fafb
EduM22 <38257387+EduM22@users.noreply.github.com>
2025-07-25 16:17:40 +0200

    add toml file upload

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # hilly
 
-hilly is a cli for munk faas service
+hilly is a cli for [munk faas service](https://github.com/EduM22/munk-runner/)
 
 ## Install cli
 
 run this in the cmd:\
 `deno install -g -n hilly --allow-env=ESBUILD_BINARY_PATH,ESBUILD_WORKER_THREADS,DENO_TRACE_PERMISSIONS --allow-read --allow-net -c deno.json -f ./src/main.ts`
+
+## Deploy
+
+`hilly deploy <entry-point-file> --t <token> --h <domain> (optional)`\
+`hilly deploy ./munk.toml`
+
+### munk.toml template
+
+```
+[app]
+path = "./<script>.ts"
+domain = "http://localhost:3000/" (optional)
+token = "<test123>"
+```
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import * as esbuild from "esbuild";
 import { denoPlugin } from "@deno/esbuild-plugin";
+import { parse } from "@std/toml";
+import { dirname, resolve } from "@std/path";
 
-const VERSION = "v0.1.1";
+const VERSION = "v0.2.0";
 
 if (import.meta.main) {
   try {
     const { args } = Deno;
 
-    if (args.length === 0 || args.length < 4 || args.length > 6) {
+    if (args.length === 0 || args.length < 1 || args.length > 6) {
       console.log(`hilly version: ${VERSION}`);
       console.log(
         "Usage: hilly deploy <entry-point-file> --t <token> --h <domain> (optional)",
+      );
+      console.log(
+        "Usage: hilly deploy ./munk.toml",
       );
       Deno.exit(1);
     }
@@ -20,76 +25,116 @@ if (import.meta.main) {
       Deno.exit(1);
     }
 
-    const tokenArg = args.findIndex((x) => x == "--t");
-    if (tokenArg == -1) {
-      console.log("No argument for token");
-      Deno.exit(1);
-    }
-    const token = args[tokenArg + 1];
+    if (args[1].endsWith("munk.toml")) {
+      const text = await Deno.readTextFile(args[1]);
+      const obj = parse(text);
 
-    const domainArg = args.findIndex((x) => x == "--h");
-    let domain = "https://admin.ecma.run";
-    if (domainArg != -1) {
-      domain = args[domainArg + 1];
-      if (!domain.startsWith("http://") && !domain.startsWith("https://")) {
-        domain = `https://${domain}`;
-      }
-    }
+      const dir_path = dirname(args[1]);
 
-    const result = await esbuild.build({
-      entryPoints: [args[1]],
-      bundle: true,
-      minify: true,
-      write: false,
-      format: "esm",
-      plugins: [denoPlugin()],
-    });
+      const app = obj.app;
+      //@ts-expect-error not on type
+      let domain = app.domain ?? "https://admin.ecma.run";
+      //@ts-expect-error not on type
+      const token = app.token ?? "";
+      domain = domainCheck(domain);
+      //@ts-expect-error not on type
+      const app_path = app.path;
+      const path = resolve(dir_path, app_path);
 
-    if (result.errors.length > 0) {
-      console.error(`Errors from bundle: `);
-      result.errors.forEach((error) => {
-        console.error(error);
-      });
-
-      Deno.exit(1);
+      const code = await bundle(path);
+      await upload(code, domain, token);
     } else {
-      if (result.warnings.length > 0) {
-        console.warn(`Warnings from bundle:`);
-        result.warnings.forEach((warning) => {
-          console.warn(warning);
-        });
+      const tokenArg = args.findIndex((x) => x == "--t");
+      if (tokenArg == -1) {
+        console.log("No argument for token");
+        Deno.exit(1);
+      }
+      const token = args[tokenArg + 1];
+
+      const domainArg = args.findIndex((x) => x == "--h");
+      let domain = "https://admin.ecma.run";
+      if (domainArg != -1) {
+        domain = args[domainArg + 1];
+        domain = domainCheck(domain);
       }
 
-      if (result.outputFiles[0]) {
-        const file = result.outputFiles[0];
-        const source = file.text.replaceAll('"', "'");
-        const link = `${domain}/api/function`;
-        const response = await fetch(link, {
-          method: "POST",
-          headers: {
-            "munk-auth": token,
-          },
-          body: JSON.stringify({
-            code: source,
-          }),
-        });
-
-        if (!response.ok) {
-          console.error(
-            `Could not upload the code | status: ${response.status}, ${response.statusText}, ${await response
-              .text()}`,
-          );
-          Deno.exit(1);
-        }
-
-        const functionId = response.headers.get("munk-function-id");
-
-        console.log(`👋 New function created with id: ${functionId}`);
-      } else {
-        console.error("No file");
-      }
+      const code = await bundle(args[1]);
+      await upload(code, domain, token);
     }
   } catch (ex) {
     console.error(ex);
   }
+}
+
+async function bundle(file: string): Promise<string> {
+  const result = await esbuild.build({
+    entryPoints: [file],
+    bundle: true,
+    minify: true,
+    write: false,
+    format: "esm",
+    plugins: [denoPlugin()],
+  });
+
+  if (result.errors.length > 0) {
+    console.error(`Errors from bundle: `);
+    result.errors.forEach((error) => {
+      console.error(error);
+    });
+
+    Deno.exit(1);
+  } else {
+    if (result.warnings.length > 0) {
+      console.warn(`Warnings from bundle:`);
+      result.warnings.forEach((warning) => {
+        console.warn(warning);
+      });
+    }
+
+    if (result.outputFiles[0]) {
+      const file = result.outputFiles[0];
+      const source = file.text.replaceAll('"', "'");
+
+      return source;
+    } else {
+      console.error("No file");
+      Deno.exit(1);
+    }
+  }
+}
+
+async function upload(code: string, domain: string, token: string) {
+  const link = `${domain}/api/function`;
+  const response = await fetch(link, {
+    method: "POST",
+    headers: {
+      "munk-auth": token,
+    },
+    body: JSON.stringify({
+      code: code,
+    }),
+  });
+
+  if (!response.ok) {
+    console.error(
+      `Could not upload the code | status: ${response.status}, ${response.statusText}, ${await response
+        .text()}`,
+    );
+    Deno.exit(1);
+  }
+  const functionId = response.headers.get("munk-function-id");
+
+  console.log(`👋 New function created with id: ${functionId}`);
+}
+
+function domainCheck(domain: string): string {
+  if (!domain.startsWith("http://") && !domain.startsWith("https://")) {
+    domain = `https://${domain}`;
+  }
+
+  if (domain.endsWith("/")) {
+    domain = domain.slice(0, -1);
+  }
+
+  return domain;
 }
